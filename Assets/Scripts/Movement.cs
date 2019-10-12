cc1a24c23a05e38b35210445073bf16d0b0968db
PandaGears <37896398+PandaGears@users.noreply.github.com>
2019-10-12 14:27:03 +0200

    Add files via upload

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,31 +4,48 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-public float speed = 5F;
+   	public float speed;
+	private Rigidbody2D rBody;
+	private Vector2 moveVelocity;
+	public LayerMask ObstacleMask;
     void Start()
     {
-        
+        rBody = GetComponent<Rigidbody2D>();
     }
-	void changePosition(Vector3 direction) {
-		float step = speed * Time.deltaTime;
-		transform.position = Vector3.MoveTowards (transform.position, transform.position + direction, step);
-	
-	}
     void Update()
     {
-		Vector3 Move = Vector3.zero;
-		if (Input.GetKey (KeyCode.W))
-			Move += Vector3.up;			
-		if (Input.GetKey (KeyCode.S))
-			Move += Vector3.down;			
-		if (Input.GetKey (KeyCode.A))
-			Move += Vector3.left;			
-		if (Input.GetKey (KeyCode.D))
-			Move += Vector3.right;
-        		changePosition (Move);
+		Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		moveVelocity = moveInput.normalized * speed;
         Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
 		diff.Normalize();
 		float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
-		transform.rotation = Quaternion.Euler(0f, 0f, rot_z + 90);  
-    }
+		transform.rotation = Quaternion.Euler(0f, 0f, rot_z + 90);
+
+
+		// Vector3 Move = Vector3.zero;
+		// if (Input.GetKey (KeyCode.W))
+		// 	Move += Vector3.up;			
+		// if (Input.GetKey (KeyCode.S))
+		// 	Move += Vector3.down;			
+		// if (Input.GetKey (KeyCode.A))
+		// 	Move += Vector3.left;			
+		// if (Input.GetKey (KeyCode.D))
+		// 	Move += Vector3.right;
+
+		// if (Move == Vector3.zero)
+		// 	legs.GetComponent<Animator> ().SetBool ("isMoving", false);
+		// else
+		// 	changePosition (Move);
+
+			
+	}
+	// 	void changePosition(Vector3 direction) {
+	// 	float step = speed * Time.deltaTime;
+	// 	transform.position = Vector3.MoveTowards (transform.position, transform.position + direction, step);
+	// 	legs.GetComponent<Animator>().SetBool ("isMoving", true);
+	// }
+
+	void FixedUpdate() {
+		rBody.MovePosition(rBody.position + moveVelocity * Time.fixedDeltaTime);
+	}
 }
