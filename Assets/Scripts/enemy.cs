a818f662de3c95d3ffcbf69e01459da9737dad6a
PandaGears <37896398+PandaGears@users.noreply.github.com>
2019-10-13 08:02:25 +0200

    Add files via upload

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    public float noticeDistance;
+    private Transform playerPos;
+    void Start()
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    
+    void Update()
+    {
+                if(Vector2.Distance(transform.position, playerPos.position) > noticeDistance){        {
+                   if(Vector2.Distance(transform.position, playerPos.position) > stoppingDistance){
+                        transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+                    }
+                }   
+            }
+}
+}
