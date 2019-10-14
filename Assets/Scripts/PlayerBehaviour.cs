19304f049774f19a4ea1156dba7253f5ecb66eaa
glatega <38347479+glatega@users.noreply.github.com>
2019-10-14 13:11:26 +0200

    Add files via upload

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,53 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+
 public class PlayerBehaviour : MonoBehaviour
 {
-    private bool haswon = false;
-    private bool haslost = false;
-        public AudioClip death;
-    public AudioSource aSource;
     [SerializeField]
-    public int health = 1; 
-
     public Weapon weapon;
-<<<<<<< HEAD
     public Collider2D droppedWeapon;
     public int health = 1;
 
-=======
-    public Text Ammodeets;
-
-    public Text weapondeets;
-
-    [SerializeField]
-    private Transform GunTip;
-    private Rigidbody2D rb2d;
-    [SerializeField]
-    public GameObject bullet;
-
-    public Vector2 bulletPos;
-    public Collider2D droppedWeapon;
-    // public float fireRate = 0.5f;
-    // public float nextShot = 0.0f;
-    public GameObject effect;
->>>>>>> 2e8b7fd7c07cb0e3cac670efda3e8a2f53904f21
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        
     }
 
     void Update()
     {
-        if (weapon == null){
-            weapondeets.text = "WEAPON : ";
-		    Ammodeets.text = "AMMO : ";
-            } 
         if (Input.GetKey(KeyCode.Mouse0) && weapon != null) {
-            weapondeets.text = "WEAPON : " + weapon;
-		    Ammodeets.text = "AMMO : " + weapon.ammo; 
             weapon.fire();
         }
 
@@ -59,12 +28,7 @@ public class PlayerBehaviour : MonoBehaviour
             if (droppedWeapon != null && weapon == null) {
                 weapon = droppedWeapon.GetComponent<Weapon>();
                 weapon.equip(gameObject);
-    
-    
             }
-        }
-        if (GameObject.FindGameObjectsWithTag("Enemy").Length <= 0) {
-            FindObjectOfType<GameManager>().GenocideLevel();
         }
     }
 
@@ -81,31 +45,6 @@ public class PlayerBehaviour : MonoBehaviour
             } else {
                 droppedWeapon = collider;
             }
-        }
-       else if(collider.gameObject.tag == "Enemy"){
-            health--;
-            Instantiate(effect, transform.position, Quaternion.identity);
-             Debug.Log(health);
-            if (health <= 0){
-                aSource.PlayOneShot(death);
-                aSource.Play();
-                rb2d.constraints = RigidbodyConstraints2D.FreezeAll;
-                FindObjectOfType<GameManager>().LostLevel();
-            }
-        }
-    //            else if(collider.gameObject.tag == "BulletEnemy"){
-    //         Instantiate(effect, transform.position, Quaternion.identity);
-    //          health--;
-    //          Debug.Log(health);
-    //         if (health == 0){
-    //         aSource.PlayOneShot(death);
-    //         aSource.Play();
-    //         Destroy(gameObject);
-    //    }
-    //     }
-        else if (collider.gameObject.tag == "goal") {
-                FindObjectOfType<GameManager>().CompleteLevel();
-            haswon = true;
         }
     }
 
